17d7f3e45517fe1c53b86def20a8bbddb96e20d8
gomjellie <gomjellie@naver.com>
2017-11-14 17:14:12 +0900

    getSsuDormUrl dow 인자 제거, return 폼 변경

--- example/foodParser.js
+++ example/foodParser.js
@@ -1,57 +1,62 @@
 'use strict';
 
 const sandCastle = require('../index');
+const util = require('util');
+
+const myLogger = (res) => {
+  console.log(util.inspect(res, {showHidden: false, depth: null}));
+};
 
 function test() {
   let option = {
-    dayOfWeek: 'thu',
+    dayOfWeek: sandCastle.Cafeteria.DayOfWeek.TUE,
     kind: sandCastle.Cafeteria.Kind.STUDENT,
   };
   sandCastle.cafeteria(option)
-    .then(console.log);
+    .then(myLogger);
 
   option = {
-    dayOfWeek: 4,
+    dayOfWeek: 2,
     kind: sandCastle.Cafeteria.Kind.STUDENT,
   };
   sandCastle.cafeteria(option)
-    .then(console.log);
+    .then(myLogger);
 
   option = {
-    dayOfWeek: sandCastle.Cafeteria.DayOfWeek.THU,
+    dayOfWeek: sandCastle.Cafeteria.DayOfWeek.TUE,
     kind: sandCastle.Cafeteria.Kind.OFFICER,
   };
   sandCastle.cafeteria(option)
-    .then(console.log);
+    .then(myLogger);
 
   option = {
-    date: '2017-11-04',
+    date: '2017-11-14',
     kind: sandCastle.Cafeteria.Kind.RESIDENCE,
   };
   sandCastle.cafeteria(option)
-    .then(console.log);
+    .then(myLogger);
 
   option = {
-    dayOfWeek: sandCastle.Cafeteria.DayOfWeek.THU,
+    dayOfWeek: sandCastle.Cafeteria.DayOfWeek.TUE,
     kind: sandCastle.Cafeteria.Kind.RESIDENCE,
   };
   sandCastle.cafeteria(option)
-    .then(console.log);
+    .then(myLogger);
 
   option = {
-    dayOfWeek: sandCastle.Cafeteria.DayOfWeek.THU,
+    dayOfWeek: sandCastle.Cafeteria.DayOfWeek.TUE,
     kind: sandCastle.Cafeteria.Kind.RESIDENCE,
   };
   sandCastle.cafeteria(option)
-    .then(console.log);
+    .then(myLogger);
 
   option = {
     dayOfWeek: '2016-00', // 작동안함
     kind: sandCastle.Cafeteria.Kind.RESIDENCE,
   };
   sandCastle.cafeteria(option)
-    .then(console.log)
-    .catch(console.log);
+    .then(myLogger)
+    .catch(myLogger);
 }
 
 test();
